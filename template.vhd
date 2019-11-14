af3e80b0942dfec9100e98368278566ab43ebdbd
Theodor Fragner <theo@fragner.org>
2019-11-14 08:36:31 +0100

    initial commit

---
+++ template.vhd
@@ -0,0 +1,19 @@
+-- name:    [project].vhd
+-- author:  [author]
+-- date:    [date]
+-- content: [description]
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_arith.all;
+use ieee.std_logic_unsigned.all;
+use ieee.math_real.all;
+
+entity [project] is
+  port(RESET_n : in std_logic;
+       CLK     : in std_logic);
+end [project];
+
+architecture rtl of [project] is
+begin
+end rtl;
